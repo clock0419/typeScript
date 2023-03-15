82ad91a33873bd333310fd23b8633d362699dbb0
clock0419 <kangdongho0419@gmail.com>
2023-03-15 23:42:47 +0900

    2

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -1,13 +1,27 @@
 // js 문자열 선언
 var str = 'hello';
 
-// ts 문자열 선언
+// TS 문자열 선언
 let str2: string = 'string';
 
-// ts 숫자
+// TS 숫자
 let num: number = 123;
 
-// ts 배열
+// TS 배열
 let arr: Array<number> = [1,2,3];
 let heroes: Array<string> = ['cpt', 'thor', 'hulk'];
 let items: number[] = [1,2,3];
+
+// TS 튜플
+let address: [string, number] = ['gangnam', 123];
+
+// TS 객체 
+let obj: object = {};
+
+let person: { name:string, age:number} = {
+    name: 'thor',
+    age:1000   
+}
+
+// 블론
+let show: boolean = true;
